f46755a5e77ec50d6e13b567b84d9f1331493d57
JoaoFigueiro <joao.l.comiran@gmail.com>
2024-08-09 23:18:57 -0300

    FEAT lab

--- instances.py
+++ instances.py
@@ -101,3 +101,22 @@ print('Total number of mobile phones created:', MobilePhone.last_SN)
 print(fphone.call('01632-960004'))
 print('Fixed phone received "{}" serial number'.format(fphone.SN))
 print('Mobile phone received "{}" serial number'.format(mphone.SN))
+
+# lab
+import random
+
+class Apple:
+    counter = 0
+    total_weight = 0
+
+    def __init__(self, weight):
+        self.weight = weight
+        Apple.total_weight += weight
+        Apple.counter += 1
+
+while Apple.counter < 1000 and Apple.total_weight < 300:
+    apple = Apple(random.uniform(0.2, 0.5))
+
+print('A limit has been reached. The order details:')
+print('# of apples:', Apple.counter)
+print('total weight:', round(Apple.total_weight, 2))
