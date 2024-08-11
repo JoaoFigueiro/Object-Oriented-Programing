4716e99ca5a7a8d98e0664880b1d2f500b663e85
JoaoFigueiro <joao.l.comiran@gmail.com>
2024-08-11 19:43:13 -0300

    FEAT core syntax lab structure

---
+++ core_syntax_lab.py
@@ -0,0 +1,19 @@
+import datetime
+
+class TimeInterval: 
+    def __init__(self, hours, minutes, seconds): 
+        self.hours = hours
+        self.minutes = minutes
+        self.secondes = seconds 
+
+    def __add__(self): 
+        pass  
+
+    def __sub__(self):
+        pass 
+
+    def __mul__(self, value): 
+        pass
+
+    def __str__(self): 
+        return 
