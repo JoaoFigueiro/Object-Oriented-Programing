6f8047f7feafd8f53f12af72d7bdd3769d389eb9
João Leonardo Comiran Figueiró <93953665+JoaoFigueiro@users.noreply.github.com>
2024-08-20 23:47:14 -0300

    FEAT decorator as a class

--- decorators.py
+++ decorators.py
@@ -46,3 +46,22 @@ def pack_fruits(*args):
 pack_books('Alice in Wonderland', 'Winnie the Pooh')
 pack_toys('doll', 'car')
 pack_fruits('plum', 'pear')
+
+class SimpleDecorator:
+    def __init__(self, own_function):
+        self.func = own_function
+
+    def __call__(self, *args, **kwargs):
+        print('"{}" was called with the following arguments'.format(self.func.__name__))
+        print('\t{}\n\t{}\n'.format(args, kwargs))
+        self.func(*args, **kwargs)
+        print('Decorator is still operating')
+
+
+@SimpleDecorator
+def combiner(*args, **kwargs):
+    print("\tHello from the decorated function; received arguments:", args, kwargs)
+
+
+combiner('a', 'b', exec='yes')
+
