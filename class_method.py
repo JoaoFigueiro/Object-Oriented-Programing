576c2b8ff569af3dd5e37f1c796c4fe83170fd7a
João Leonardo Comiran Figueiró <93953665+JoaoFigueiro@users.noreply.github.com>
2024-08-22 23:57:02 -0300

    FEAT alternative constructor

--- class_method.py
+++ class_method.py
@@ -16,3 +16,24 @@ print(Example.get_internal())
 example2 = Example(99)
 print(Example.get_internal())
 print(example2.get_internal())
+
+# constructor validation
+
+class Car:
+    def __init__(self, vin):
+        print('Ordinary __init__ was called for', vin)
+        self.vin = vin
+        self.brand = ''
+
+    @classmethod
+    def including_brand(cls, vin, brand):
+        print('Class method was called')
+        _car = cls(vin)
+        _car.brand = brand
+        return _car
+
+car1 = Car('ABCD1234')
+car2 = Car.including_brand('DEF567', 'NewBrand')
+
+print(car1.vin, car1.brand)
+print(car2.vin, car2.brand)
